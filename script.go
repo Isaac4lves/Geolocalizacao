cf62f527fa8bfc3dd037edcdc7e9ec12ce966892
Isaac🎲 <cassioitavom@gmail.com>
2024-12-20 18:08:38 -0300

    Update script.go
    Latitude e Longitude

--- script.go
+++ script.go
@@ -3,15 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 type IpDatas struct {
-	IP         string `json:"query"`
-	Pais       string `json:"country"`
-	CodigoPais string `json:"countryCode"`
-	Estado     string `json:"regionName"`
-	Municipio  string `json:"city"`
+	IP         string  `json:"query"`
+	Pais       string  `json:"country"`
+	CodigoPais string  `json:"countryCode"`
+	Estado     string  `json:"regionName"`
+	Municipio  string  `json:"city"`
+	Latitude   float64 `json:"lat"`
+	Longitude  float64 `json:"lon"`
 }
 
 func RequestApi(ip string, tipo string) string {
@@ -46,7 +50,34 @@ func RequestApi(ip string, tipo string) string {
 	if tipo == "codigoPais" {
 		return dados.CodigoPais
 	}
-
+	if tipo == "latitude" {
+		// Aqui, utilizamos a função strconv.FormatFloat, que recebe quatro parâmetros:
+		// o número a ser convertido (valor), o formato ('f' para ponto fixo), a precisão
+		// (-1 para usar a precisão mínima necessária para representar o valor sem perda de dados)
+		//  e o tamanho em bits do número (64 para especificar que o número é do tipo float64).
+		//  Essa função retorna o valor formatado como uma string, que é então armazenado na variável strValor.
+		lat := strconv.FormatFloat(dados.Latitude, 'f', -1, 64)
+		return lat
+	}
+	if tipo == "longitude" {
+		lon := strconv.FormatFloat(dados.Longitude, 'f', -1, 64)
+		return lon
+	}
 	return "\nfim"
 
+}
+
+func Getip() string {
+	response, err := http.Get("https://meuip.com/api/meuip.php")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Erro ao ler resposta:", err)
+	}
+	ip := string(body)
+	return ip
 }
